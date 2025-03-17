a7c38b4cf7e748a057d048ced76510855c358f5c
Ivan Beusan <ibeusan20@foi.hr>
2025-03-17 19:26:19 +0100

    Odrađena vježba 2

---
+++ ibeusan20_vjezba_02/ibeusan20_vjezba_02/src/main/java/edu/unizg/foi/nwtis/ibeusan20/ibeusan20_vjezba_02/App.java
@@ -0,0 +1,13 @@
+package edu.unizg.foi.nwtis.ibeusan20.ibeusan20_vjezba_02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
