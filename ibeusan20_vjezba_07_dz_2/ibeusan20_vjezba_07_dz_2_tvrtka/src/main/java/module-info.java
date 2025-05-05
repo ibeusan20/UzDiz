e41ccf1132bc9d3c96778b3a59472ba59e52d1dc
Ivan Beusan <ibeusan20@foi.hr>
2025-05-05 19:27:01 +0200

    vjezba_07_dz_2 zavrsno s tockom 24.(ukljucujuci)

---
+++ ibeusan20_vjezba_07_dz_2/ibeusan20_vjezba_07_dz_2_tvrtka/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ibeusan20_vjezba_07_dz_2_tvrtka {
+  exports edu.unizg.foi.nwtis.ibeusan20.vjezba_07_dz_2;
+
+  requires com.google.gson;
+  requires ibeusan20_vjezba_07_dz_2_lib_konfig;
+  requires ibeusan20_vjezba_07_dz_2_lib_podaci;
+  requires java.logging;
+}
