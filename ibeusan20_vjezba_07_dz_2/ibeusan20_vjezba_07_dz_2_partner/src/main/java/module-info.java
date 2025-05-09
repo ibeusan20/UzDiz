237243fd852cfb5a382b6a30238ec142683eb691
Ivan Beusan <ibeusan20@foi.hr>
2025-05-09 12:05:13 +0200

    napokon gotova vježba 07 s točkom 29. uključujući 29.

---
+++ ibeusan20_vjezba_07_dz_2/ibeusan20_vjezba_07_dz_2_partner/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module ibeusan20_vjezba_07_dz_2_partner {
+  exports edu.unizg.foi.nwtis.ibeusan20.vjezba_07_dz_2;
+
+  requires com.google.gson;
+  requires ibeusan20_vjezba_07_dz_2_lib_konfig;
+  requires ibeusan20_vjezba_07_dz_2_lib_podaci;
+}
