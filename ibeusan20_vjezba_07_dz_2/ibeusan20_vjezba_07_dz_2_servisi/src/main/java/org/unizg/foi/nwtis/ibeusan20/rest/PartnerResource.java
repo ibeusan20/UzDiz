3033bec923fbc72423dc423e0ed8af1b22595883
Ivan Beusan <ibeusan20@foi.hr>
2025-05-18 21:06:18 +0200

    vj 07 drugi tjedan do 71. točke uljkučujući nju

---
+++ ibeusan20_vjezba_07_dz_2/ibeusan20_vjezba_07_dz_2_servisi/src/main/java/org/unizg/foi/nwtis/ibeusan20/rest/PartnerResource.java
@@ -0,0 +1,14 @@
+package org.unizg.foi.nwtis.ibeusan20.rest;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
+
+@Path("api/partner")
+public class PartnerResource {
+
+}
