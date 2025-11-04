5ba42c1b90ae840064403af556b5f156be4ddf8d
Ivan Beusan <ibeusan20@foi.hr>
2025-11-04 22:05:13 +0100

    vraćeno s nwtisa tvrtka u zadaću 3, pronašao ju u smeću

---
+++ ibeusan20_vjezba_08_dz_3/ibeusan20_vjezba_08_dz_3_tvrtka/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module ibeusan20_vjezba_08_dz_3_tvrtka {
+  exports edu.unizg.foi.nwtis.ibeusan20.vjezba_08_dz_3;
+
+  requires com.google.gson;
+  requires ibeusan20_vjezba_08_dz_3_lib_konfig;
+  requires ibeusan20_vjezba_08_dz_3_lib_podaci;
+  requires java.logging;
+  requires java.net.http;
+}
