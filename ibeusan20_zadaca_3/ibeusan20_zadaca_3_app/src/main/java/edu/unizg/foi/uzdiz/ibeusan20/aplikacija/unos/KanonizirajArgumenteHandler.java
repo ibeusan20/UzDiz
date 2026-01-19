81e38299c34c6fa86251ed013697e05f49a6e7dc
Ivan Beusan <ibeusan20@foi.hr>
2026-01-19 05:32:00 +0100

    updated readme

--- ibeusan20_zadaca_3/ibeusan20_zadaca_3_app/src/main/java/edu/unizg/foi/uzdiz/ibeusan20/aplikacija/unos/KanonizirajArgumenteHandler.java
+++ ibeusan20_zadaca_3/ibeusan20_zadaca_3_app/src/main/java/edu/unizg/foi/uzdiz/ibeusan20/aplikacija/unos/KanonizirajArgumenteHandler.java
@@ -8,12 +8,12 @@ final class KanonizirajArgumenteHandler extends ApstraktniHandlerUnosa {
 
     String cmd = ctx.naredba;
 
-    // Argument 0 je "mod" (A/R) za ove komande:
+    // Argument 0 je argument (A/R) za ove komande:
     if ((cmd.equals("BP") || cmd.equals("UP") || cmd.equals("PPTAR")) && ctx.argumenti.size() >= 1) {
       ctx.argumenti.set(0, ctx.argumenti.get(0).trim().toUpperCase());
     }
 
-    // IRTA: drugi argument (vrste) u uppercase
+    // IRTA: drugi argument npr PAČO u uppercase
     if (cmd.equals("IRTA") && ctx.argumenti.size() >= 2) {
       ctx.argumenti.set(1, ctx.argumenti.get(1).trim().toUpperCase());
     }
