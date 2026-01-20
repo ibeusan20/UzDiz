0c8ebebb85bb3dadf22055c08483861ef033ffe0
Ivan Beusan <ibeusan20@foi.hr>
2026-01-20 01:21:34 +0100

    skripta za pokretanje

--- UzDiz_helper/DZX_pokretanje/run_file.sh
+++ UzDiz_helper/DZX_pokretanje/run_file.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# 1) Build projekta
+cd /home/NWTiS_2/ibeusan20/ibeusan20_zadaca_3
+mvn clean install
+
+# 2) Vraćanje u mapu gdje je run_file.sh
+cd /home/NWTiS_2/Desktop/DZX_pokretanje
+
 #JAR="/home/NWTiS_2/ibeusan20/ibeusan20_zadaca_2/ibeusan20_zadaca_2_app/target/ibeusan20_zadaca_2.jar"
 JAR="/home/NWTiS_2/ibeusan20/ibeusan20_zadaca_3/ibeusan20_zadaca_3_app/target/ibeusan20_zadaca_3.jar"
 
-#ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije.csv)
-#ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije.csv --jdr)
-#ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije.csv --vdr)
-#ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije.csv --jdr --vdr)
-ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije.csv)
+#ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije_1.csv)
+#ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije_1.csv --jdr)
+#ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije_1.csv --vdr)
+#ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije_1.csv --jdr --vdr)
+
+ARGS=(--ta DZ_1_aranzmani.csv --rta DZ_1_rezervacije.csv --jdr)
 
 INTERVAL="0.2"
 
